9be9f3bbc6193bf169dbcbb8a738b51e3ab43ab8
Garrick Larson Potts <ricki.glp@gmail.com>
2024-01-31 11:12:48 -0800

    tasks sudo command added

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -13,6 +13,7 @@ env.hosts = ['54.146.82.137', '54.237.106.250']
 env.user = 'ubuntu'
 env.key_filename = '~/.ssh/id_rsa'
 
+
 def do_pack():
     """Packs web_static files into .tgz archive"""
     try:
@@ -21,8 +22,9 @@ def do_pack():
         local("sudo mkdir -p versions")
         local("sudo tar -cvzf {} web_static".format(archive_path))
         return archive_path
-    except:
+    except pass:
         return None
+
 
 def do_deploy(archive_path):
     """Deploys the web static"""
@@ -34,21 +36,23 @@ def do_deploy(archive_path):
 
     try:
         put(archive_path, '/tmp/')
-        run('mkdir -p /data/web_static/releases/{}/'.format(archive_no_ext))
-        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'
+        run('sudo mkdir -p /data/web_static/releases/{}/'
+            .format(archive_no_ext))
+        run('sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/'
             .format(archive_name, archive_no_ext))
-        run('rm /tmp/{}'.format(archive_name))
-        run('mv /data/web_static/releases/{}/web_static/* \
+        run('sudo rm /tmp/{}'.format(archive_name))
+        run('sudo mv /data/web_static/releases/{}/web_static/* \
             /data/web_static/releases/{}/'
             .format(archive_no_ext, archive_no_ext))
-        run('rm -rf /data/web_static/releases/{}/web_static'
+        run('sudo rm -rf /data/web_static/releases/{}/web_static'
             .format(archive_no_ext))
-        run('rm -rf /data/web_static/current')
-        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'
+        run('sudo rm -rf /data/web_static/current')
+        run('sudo ln -s /data/web_static/releases/{}/ /data/web_static/current'
             .format(archive_no_ext))
         return True
-    except:
+    except pass:
         return False
+
 
 def deploy():
     """Packs the web static files then deploys them"""
