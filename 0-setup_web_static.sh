7ec29692ac407e47e937cbb564c8bda2da060b78
Garrick Larson Potts <ricki.glp@gmail.com>
2024-01-31 11:34:50 -0800

    task 0 attempting corrrections

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,29 +1,17 @@
 #!/usr/bin/env bash
 # This scripts sets up your web servers for the deployment of web_static
-if ! [ -x "$(command -v nginx)" ]; then
-        sudo apt-get update -y && sudo apt upgrade -y
+if ! command -v nginx &> /dev/null
+then
+        sudo apt-get update && sudo apt-get upgrade -y
         sudo apt-get install nginx -y
 fi
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/
-
 sudo echo "Hello, World!" > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 sudo chown -R ubuntu:ubuntu /data/
-
-sudo rm /etc/nginx/sites-available/default
-sudo echo "server {
-        listen 80 default_server;
-        listen [::]:80 default_server;
-        root /var/www/html;
-        index index.html;
-        server_name _;
-        location / {
-                try_files $uri $uri/ -404;
-        }
-}"
+sudo sed -i '/listen 80 default_server;/a location /hbnb_static/ {\n\talias \
+	     /data/web_static/current/;\n}\n' \
+	     /etc/nginx/sites-available/default
 sudo service nginx restart
