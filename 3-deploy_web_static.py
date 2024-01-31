ed3f4eb4d1156549155e349202438dd040cc1fee
Garrick Larson Potts <ricki.glp@gmail.com>
2024-01-31 10:25:33 -0800

    task 3 Full deployment

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""
+Fabric script that creates and distributes an archive to your web servers,
+using the function deploy
+"""
+
+from fabric.api import env, local, run
+from os.path import exists
+from datetime import datetime
+
+
+env.hosts = ['54.146.82.137', '54.237.106.250']
+env.user = 'ubuntu'
+env.key_filename = '~/.ssh/id_rsa'
+
+def do_pack():
+    """Packs web_static files into .tgz archive"""
+    try:
+        current_time = datetime.utcnow().strftime('%Y%m%d%H%M%S')
+        archive_path = "versions/web_static_{}.tgz".format(current_time)
+        local("mkdir -p versions")
+        local("tar -cvzf {} web_static".format(archive_path))
+        return archive_path
+    except:
+        return None
+
+def do_deploy(archive_path):
+    """Deploys the web static"""
+    if not exists(archive_path):
+        return False
+
+    archive_name = archive_path.split("/")[-1]
+    archive_no_ext = archive_name.split(".")[0]
+
+    try:
+        put(archive_path, '/tmp/')
+        run('mkdir -p /data/web_static/releases/{}/'.format(archive_no_ext))
+        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'
+            .format(archive_name, archive_no_ext))
+        run('rm /tmp/{}'.format(archive_name))
+        run('mv /data/web_static/releases/{}/web_static/* \
+            /data/web_static/releases/{}/'
+            .format(archive_no_ext, archive_no_ext))
+        run('rm -rf /data/web_static/releases/{}/web_static'
+            .format(archive_no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'
+            .format(archive_no_ext))
+        return True
+    except:
+        return False
+
+def deploy():
+    """Packs the web static files then deploys them"""
+    archive_path = do_pack()
+    if archive_path is None:
+        return False
+    return do_deploy(archive_path)
