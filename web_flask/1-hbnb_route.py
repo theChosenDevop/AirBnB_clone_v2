95d04b987fe3474b09423848f2a242e1e1466ae9
Garrick Larson Potts <ricki.glp@gmail.com>
2024-02-16 06:04:08 -0800

    task 1

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""This is the 1-hbnb_route module"""
+
+from flask import Flask
+"""The imported modules are defined here"""
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
