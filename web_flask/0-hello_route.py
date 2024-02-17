bc221ed90c426cb4f744e44d7c9b92b18f9a3a11
theChosenDevop <tobbey.adesanya@gmail.com>
2024-02-17 09:09:06 -0800

    home route

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-"""This is the 0-hello_route module"""
+""" This is the 0-hello_route module """
 
 from flask import Flask
-"""Imports the module(s) or method(s)"""
-
+""" import Flask module from flask package """
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello_flask():
+def hello_hbnb():
+    """ returns a string output """
     return "Hello HBNB!"
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
