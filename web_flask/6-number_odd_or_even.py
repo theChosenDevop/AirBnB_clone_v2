732b15ce3f0504cc3540e31cd6960a2266217fdb
theChosenDevop <tobbey.adesanya@gmail.com>
2024-02-18 16:34:17 -0800

    rendered html with dynamic inout

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -46,5 +46,16 @@ def number_template(n):
         return render_template('5-number.html', n=n)
 
 
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_odd_or_even(n):
+    """ Render html template if it is odd or even """
+    if isinstance(n, int):
+        if n % 2 == 0:
+            return render_template('6-number_odd_or_even.html', n=n, num='even')
+        else:
+            return render_template('6-number_odd_or_even.html', n=n, num='odd')
+
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
